9e1a40a0c50f71a127b5adf099f01b9a1b63aa4a
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2022-09-03 22:07:03 +0900

    .zshrc及びsheldonの設定ファイルを追加

---
+++ local_plugins/bindkey.zsh
@@ -0,0 +1,3 @@
+bindkey '^R' anyframe-widget-execute-history
+bindkey '^E' anyframe-widget-cdr
+bindkey '^G' anyframe-widget-checkout-git-branch
---
+++ local_plugins/history.zsh
@@ -0,0 +1,3 @@
+HIST_STAMPS="yyyy-mm-dd"
+setopt hist_ignore_all_dups # 過去の重複した履歴を削除
+setopt hist_reduce_blanks # 余計な空白を削除
