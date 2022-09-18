5cf61ab62c9bd6ca5146fb257bbdbe4ffe95583b
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2022-09-18 19:28:14 +0900

    bindkey.zshを修正

--- local_plugins/bindkey.zsh
+++ local_plugins/bindkey.zsh
@@ -1,3 +1,6 @@
-bindkey '^R' anyframe-widget-execute-history
+# コマンドライン履歴から選んでコマンドラインに挿入する
+bindkey '^R' anyframe-widget-put-history
+# 過去に移動したことのあるディレクトリに移動する
 bindkey '^E' anyframe-widget-cdr
+# Gitブランチを切り替える
 bindkey '^G' anyframe-widget-checkout-git-branch
