e127b1d47c6947bc327672581da63adea33984af
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2023-05-14 17:20:53 +0900

    [fix] テーマをsubmoduleで管理するよう変更

--- bin/install.zsh
+++ bin/install.zsh
@@ -3,16 +3,19 @@
 MYZSH_PATH=$(realpath $(dirname $0)/..)
 SHELDON_PATH=$HOME/.config/sheldon
 
-## .zshrcのシンボリックリンクを作成
+### .zshrcのシンボリックリンクを作成
 ln -fsv $MYZSH_PATH/.zshrc $HOME
 
-## plugins.tomlのシンボリックリンクリンクを作成
+### plugins.tomlのシンボリックリンクリンクを作成
 mkdir -pv $SHELDON_PATH
 ln -fsv $MYZSH_PATH/plugins.toml $SHELDON_PATH
 
-## local-asyncのシンボリックリンクを作成
+### conf.dのシンボリックリンクを作成
 mkdir -pv $SHELDON_PATH/conf.d
 for file in $MYZSH_PATH/conf.d/*.zsh
 do
     ln -fsv $file $SHELDON_PATH/conf.d
 done
+
+### mythemeのシンボリックリンクを作成
+ln -fsv $MYZSH_PATH/mytheme $HOME/.config
